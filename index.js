caa7dacc05bfbb7c8471582786ba62ecbbd6d71e
William Gotterer <william.gotterer@gmail.com>
2021-09-20 17:07:45 -0400

    commit tree 0880908c59a5fad2354773a3d1f7002755b92bb6

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31
+let num2 = 2
+let num3 = 19
+let num4 = 5
 
-// Write your code here
+const multiply = num1 * num2
+
+let random = Math.floor(Math.random()  * 10) + 1;
+
+const mod = num3%num4
+
+const max = Math.max(5, 6, 20);
